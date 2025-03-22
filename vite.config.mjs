bb9bf8ad30b8e06847280a0d9057864806a34df2
bebeji-nappa <masadecade@gmail.com>
2025-03-23 04:16:30 +0900

    fix: tsx file

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: path.resolve(__dirname, "src/index.tsx"),
       name: "richmd.vue",
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `index.${format}.jsx`,
       types: path.resolve(__dirname, "src/index.d.ts"),
     },
     rollupOptions: {
